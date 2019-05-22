8b9c3989bfbb38e7f7e3f91633e43464d2f302bc
antonio <antonio@pykiss.com>
2019-05-22 09:07:08 +0200

    save song

---
+++ piano/src/saveSong.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { trigger, events } from './events'
+
+const ADD_SONG = gql`
+  mutation AddSongMutation($title: String!, $events: [EventInput]!) {
+    addSong(title: $title, events: $events) {
+        id
+        title
+        events{
+            activeNotes
+            duration
+        }
+    }
+}
+`
+
+class SaveSong extends Component {
+    state = {
+        tite: '',
+    }
+
+    render () {
+        return (
+            <div>
+                <input
+                    onChange={e => { this.setState({title: e.currentTarget.value})}}
+                    type="text"
+                    placeholder="The title of the song"
+                />
+                <Mutation
+                    mutation={ADD_SONG}
+                    variables={{ title: this.state.title, events: this.props.song }}
+                    onCompleted={() => trigger(events.SONG_SAVED)}
+                    onError={() => trigger(events.ERROR)}
+                >
+                    {AddSongMutation => (
+                        <button onClick={AddSongMutation}>
+                            Submit
+                        </button>
+                    )}
+                </Mutation>
+            </div>
+        )
+    }
+}
+
+export default SaveSong
