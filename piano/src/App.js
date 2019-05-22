81f91fe4d028b7de5ff199595a36716c319a0099
antonio <antonio@pykiss.com>
2019-05-22 09:05:25 +0200

    style

--- piano/src/App.js
+++ piano/src/App.js
@@ -1,82 +1,73 @@
-import React from 'react'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlayCircle, faStopCircle, faPlay, faStop, faPause } from '@fortawesome/free-solid-svg-icons'
-
-import 'react-piano/dist/styles.css'
+import React, { Component } from 'react'
 import './App.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlayCircle, faStopCircle } from '@fortawesome/free-solid-svg-icons'
 
 import MyPiano from './myPiano'
 import SongList from './SongList'
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.myPiano = React.createRef()
-  }
+import SaveSong from './saveSong';
 
+import { trigger, listen, events } from './events'
+
+class App extends Component {
+
+  componentDidMount () {
+     listen(events.NEW_SONG_RECORDED, (song) => {
+      this.song = song
+      this.setState({ status: 'SAVING' })
+    })
+
+    listen(events.SONG_SAVED, () => {
+      delete this.song
+      this.setState({ status: 'STOP' })
+    })
+
+    listen(events.ERROR, () => {
+      window.alert('ERROR')
+      this.setState({ status: 'STOP' })
+    })
+
+    listen(events.REPRODUCTION_STARTED, () => {
+      this.setState({ status: 'PLAYING' })
+    })
+
+    listen(events.REPRODUCTION_STOPED, () => {
+      this.setState({ status: 'STOP' })
+    })
+    listen(events.RECORD_STARTED, () => {
+      this.setState({ status: 'RECORDING' })
+    })
+
+  }
   state = {
     status: 'STOP'
   }
 
-  recordedSong
-
   onClickStartRecord = () => {
-    this.myPiano.current.startRecord()
-    this.setState({
-      status: 'RECORDING'
-    })
+    trigger(events.START_RECORD)
   }
   onClickStopRecord = () => {
-    const song = {
-      song: this.myPiano.current.stopRecord()
-    }
-    song.title = window.prompt('Set a title for your song (let it empty if you want to not save it)')
-
-    if (!song.title) {
-      return
-    }
-
-    console.log(this.recordedSong)
-    this.setState({
-      status: 'STOP'
-    })
-  }
-
-  onClickStartReproduction = () => {
-    this.setState({
-      status: 'PLAYING'
-    })
-    this.myPiano.current.startReproduction({events: this.recordedSong})
-  }
-  onClickStopReproduction = () => {
-    this.setState({
-      status: 'STOP'
-    })
-    this.myPiano.current.stopReproduction()
-  }
-  onClickPauseReproduction = () => {
-    this.setState({
-      status: 'PAUSED'
-    })
-    this.myPiano.current.pauseReproduction()
+    trigger(events.STOP_RECORD)
   }
 
   render () {
+    if (this.state.status === 'SAVING') {
+      return (<SaveSong song={this.song}/>)
+    }
     return (
-      <div>
-        <div id="buttons" className={this.state.status}>
-          <button className="start record" onClick={this.onClickStartRecord}><FontAwesomeIcon icon={faPlayCircle}/> Record </button>
-          <button className="stop record" onClick={this.onClickStopRecord}><FontAwesomeIcon icon={faStopCircle} /> Stop</button>
-          <button className="start reproduction" onClick={this.onClickStartReproduction}><FontAwesomeIcon icon={faPlay} /> Play</button>
-          <button className="pause reproduction" onClick={this.onClickPauseReproduction}><FontAwesomeIcon icon={faPause} /> Pause</button>
-          <button className="stop reproduction" onClick={this.onClickStopReproduction}><FontAwesomeIcon icon={faStop} /> Stop</button>
+      <div id ="app" className={this.state.status}>
+        <SongList/>
+        <div id="buttons">
+          <button className="start record" onClick={this.onClickStartRecord}>
+            <FontAwesomeIcon icon={faPlayCircle}/> Record
+          </button>
+          <button className="stop record" onClick={this.onClickStopRecord}>
+            <FontAwesomeIcon icon={faStopCircle} /> Stop Recording
+          </button>
         </div>
-        <MyPiano
-          ref={this.myPiano}
-        />
-
-        <SongList />
-
+        <div id="piano">
+          <MyPiano/>
+        </div>
       </div>
     )
   }
